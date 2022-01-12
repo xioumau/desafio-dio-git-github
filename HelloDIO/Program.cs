d9d3b686b239914a0c3dad5f8a09d1c9e05e1fb9
mauricio <mauriciocon@gmail.com>
2022-01-12 10:19:31 -0300

    Crie um programa que imprima 'Olá Digital Innovation One'

---
+++ HelloDIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloDIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Digital Innovation One!");
+        }
+    }
+}
